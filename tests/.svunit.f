ef3ba098dc73b9453fe4e14ecd14336a0c9196e1
Peter Monsson <petermonsson@opal6.opalstack.com>
2023-04-11 11:18:54 +0000

    Using runSVUnit to run tests

--- tests/.svunit.f
+++
@@ -1,8 +0,0 @@
-+incdir+tests
-+incdir+svunit/svunit_base/junit-xml
-svunit/svunit_base/junit-xml/junit_xml.sv
-+incdir+svunit/svunit_base
-svunit/svunit_base/svunit_pkg.sv
-tests/sv_waveterm_unit_test.sv
-tests/.__testsuite.sv
-tests/.testrunner.sv
