677c7ce2035011ecb5f91ab9bace1d20c13e2b65
Peter Monsson <peter@monsson.dk>
2023-04-20 09:22:52 +0200

    Fix display bugs (#22)
    * Better unit test
    
    * Fixing a display bug with clock and one with the name of a signal

--- tests/sv_waveterm_unit_test.sv
+++ tests/sv_waveterm_unit_test.sv
@@ -1,72 +1,6 @@
 `include "svunit_defines.svh"
 `include "sv_waveterm.sv"
 
-module sv_waveterm_element_unit_test;
-  import svunit_pkg::svunit_testcase;
-
-  string name = "sv_waveterm_element_ut";
-  svunit_testcase svunit_ut;
-
-
-  //===================================
-  // This is the UUT that we're 
-  // running the Unit Tests on
-  //===================================
-  sv_waveterm_element my_sv_waveterm_element;
-
-
-  //===================================
-  // Build
-  //===================================
-  function void build();
-    svunit_ut = new(name);
-
-    my_sv_waveterm_element = new("counter", 32);
-  endfunction
-
-
-  //===================================
-  // Setup for running the Unit Tests
-  //===================================
-  task setup();
-    svunit_ut.setup();
-    /* Place Setup Code Here */
-
-  endtask
-
-
-  //===================================
-  // Here we deconstruct anything we 
-  // need after running the Unit Tests
-  //===================================
-  task teardown();
-    svunit_ut.teardown();
-    /* Place Teardown Code Here */
-
-  endtask
-
-
-  //===================================
-  // All tests are defined between the
-  // SVUNIT_TESTS_BEGIN/END macros
-  //
-  // Each individual test must be
-  // defined between `SVTEST(_NAME_)
-  // `SVTEST_END
-  //
-  // i.e.
-  //   `SVTEST(mytest)
-  //     <test code>
-  //   `SVTEST_END
-  //===================================
-  `SVUNIT_TESTS_BEGIN
-
-
-
-  `SVUNIT_TESTS_END
-
-endmodule
-
 module sv_waveterm_unit_test;
   import svunit_pkg::svunit_testcase;
 
@@ -83,7 +17,6 @@ module sv_waveterm_unit_test;
   logic reset_b;
    
   logic [3:0] counter;
-
   always @(posedge clk or negedge reset_b) begin
     if (!reset_b) begin
       counter <= '0;
@@ -91,12 +24,46 @@ module sv_waveterm_unit_test;
       counter <= counter + 1'b1;
     end
   end
-
+   
 `sv_waveterm_begin(counter_waves, clk)
   `sv_waveterm_int(reset_b)
   `sv_waveterm_int(counter)
 `sv_waveterm_end
 
+  logic [31:0] big_counter_with_long_name;
+   
+  always @(posedge clk or negedge reset_b) begin
+    if (!reset_b) begin
+      big_counter_with_long_name <= '0;
+    end else begin
+      big_counter_with_long_name <= big_counter_with_long_name + 1'b1;
+    end
+  end
+
+`sv_waveterm_begin(big_counter_waves, clk)
+  `sv_waveterm_int(big_counter_with_long_name)
+  `sv_waveterm_int(reset_b)
+`sv_waveterm_end
+
+
+  wire my_clk = clk; //To detect error in get_largest_name()
+
+  logic [2:0] small_counter_with_long_name;
+   
+  always @(posedge clk or negedge reset_b) begin
+    if (!reset_b) begin
+      small_counter_with_long_name <= '0;
+    end else begin
+      small_counter_with_long_name <= small_counter_with_long_name + 1'b1;
+    end
+  end
+
+
+`sv_waveterm_begin(small_counter_waves, my_clk)
+  `sv_waveterm_int(reset_b)
+  `sv_waveterm_int(small_counter_with_long_name)
+`sv_waveterm_end
+  
   //===================================
   // Build
   //===================================
@@ -176,7 +143,7 @@ module sv_waveterm_unit_test;
       `FAIL_UNLESS_STR_EQUAL(counter_waves.sprint(), exp)
     `SVTEST_END
 
-    `SVTEST(display_9)
+    `SVTEST(display_10)
       string exp;
       exp = {
 "        +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+ +", "\n",
@@ -184,13 +151,47 @@ module sv_waveterm_unit_test;
 "         --------------------------------", "\n",
 "reset_b                                  ", "\n",
 "        +---+---+---+---+---+---+---+---+", "\n",
-"counter |2  |3  |4  |5  |6  |7  |8  |9  |", "\n",
+"counter |3  |4  |5  |6  |7  |8  |9  |a  |", "\n",
 "        +---+---+---+---+---+---+---+---+", "\n"
 };
-      repeat (10) @(posedge clk);
+      repeat (11) @(posedge clk);
       $display(counter_waves.sprint());
       `FAIL_UNLESS_STR_EQUAL(counter_waves.sprint(), exp)
     `SVTEST_END
+
+    `SVTEST(big_5)
+      string exp;
+      exp = {
+"                           +---+    +---+    +---+    +---+    +---+    +---+    +---+    +---+    +", "\n",
+"clk                        +   +----+   +----+   +----+   +----+   +----+   +----+   +----+   +----+", "\n",
+"                           +--------------------------+--------+--------+--------+--------+--------+", "\n",
+"big_counter_with_long_name |0                         |1       |2       |3       |4       |5       |", "\n",
+"                           +--------------------------+--------+--------+--------+--------+--------+", "\n",
+"                                             +------------------------------------------------------", "\n",
+"reset_b                     -----------------+                                                      ", "\n"
+};
+      repeat (6) @(posedge clk);
+      $display(big_counter_waves.sprint());
+      `FAIL_UNLESS_STR_EQUAL(big_counter_waves.sprint(), exp)
+    `SVTEST_END
+
+    `SVTEST(small_5)
+      string exp;
+      exp = {
+"                             +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+ +", "\n",
+"my_clk                       + +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+", "\n",
+"                                     +------------------------", "\n",
+"reset_b                       -------+                        ", "\n",
+"                             +-----------+---+---+---+---+---+", "\n",
+"small_counter_with_long_name |0          |1  |2  |3  |4  |5  |", "\n",
+"                             +-----------+---+---+---+---+---+", "\n"
+};
+   
+      repeat (6) @(posedge clk);
+      $display(small_counter_waves.sprint());
+      `FAIL_UNLESS_STR_EQUAL(small_counter_waves.sprint(), exp)
+    `SVTEST_END
+      
   `SVUNIT_TESTS_END
 
 endmodule
